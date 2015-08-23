19c4cfd87f8c3726b71bae3e0932e4bd8e33fb3a
Lars Kruse <devel@sumpfralle.de>
2015-08-23 17:20:17 +0200

    lots of progress

--- bdsync-manager.py
+++ bdsync-manager.py
@@ -2,22 +2,23 @@
 
 """
 
-Config file format:
+Config file format (example):
 
     [DEFAULT]
     local_bdsync_bin = /usr/local/bin/bdsync
     remote_bdsync_bin = /usr/local/bin/bdsync
     connection_command = ssh -p 2200 foo@target
     target_patch_dir = /tmp
-    bdsync_args = --hash=sha1
+    bdsync_args = --hash=sha1 --diffsize=resize
 
-    [lvm-foo/example-root]
+    [example-root]
     source_path = /dev/lvm-foo/example-root
     target_path = backup/example-root
     lvm_snapshot_enabled = yes
     lvm_snapshot_size = 5G
-    lvm_snapshot_name_suffix = _snapshot
+    lvm_snapshot_name = bdsync-snapshot
 
+The above values do not represent defaults.
 """
 
 import argparse
@@ -32,11 +33,13 @@ import time
 
 import plumbum
 
+
 LVM_SIZE_REGEX = re.compile(r"^[0-9]+[bBsSkKmMgGtTpPeE]?$")
-BDSYNC_REMOTE_TARGET_MISSING_REGEX = re.compile(r"^get_rd_queue: EOF 6$", flags=re.MULTILINE)
 
 
-class TaskProcessingError(Exception): pass
+class BDSyncManagerError(Exception): pass
+class TaskProcessingError(BDSyncManagerError): pass
+class TaskSettingsError(BDSyncManagerError): pass
 
 
 def load_settings(config):
@@ -45,46 +48,52 @@ def load_settings(config):
     try:
         settings["local_bdsync_bin"] = config["local_bdsync_bin"]
         settings["remote_bdsync_bin"] = config["remote_bdsync_bin"]
-        settings["bdsync_args"] = config.get("bdsync_args", None)
+        settings["bdsync_args"] = config.get("bdsync_args", "")
         settings["source_path"] = config["source_path"]
         settings["target_path"] = config["target_path"]
+        settings["disabled"] = config.getboolean("disabled", False)
         settings["connection_command"] = config.get("connection_command", None)
         settings["target_patch_dir"] = config.get("target_patch_dir", None)
         lvm_snapshot_enabled = config.getboolean("lvm_snapshot_enabled", False)
         if lvm_snapshot_enabled:
-            lvm_snapshot_size = config["lvm_snapshot_size"]
-            if not LVM_SIZE_REGEX.match(lvm_snapshot_size):
-                raise TaskProcessingError("Invalid LVM snapshot size (%s)" % lvm_snapshot_size)
-            lvm_snapshot_name_suffix = config.get("lvm_snapshot_name_suffix", "_snapshot")
             settings["lvm"] = {
-                    "snapshot_size": lvm_snapshot_size,
-                    "snapshot_name_suffix": lvm_snapshot_name_suffix,
+                    "snapshot_size": config["lvm_snapshot_size"],
+                    "snapshot_name": config.get("lvm_snapshot_name", "bdsync-snapshot"),
             }
     except configparser.NoOptionError as exc:
-        raise TaskProcessingError("Missing a mandatory task option: %s" % str(exc))
+        raise TaskSettingsError("Missing a mandatory task option: %s" % str(exc))
     return settings
 
 
 def validate_settings(settings):
     # validate input
     if not os.path.isfile(settings["local_bdsync_bin"]):
-        raise TaskProcessingError("The local 'bdsync' binary was not found (%s)." % settings["local_bdsync_bin"])
+        raise TaskSettingsError("The local 'bdsync' binary was not found (%s)." % settings["local_bdsync_bin"])
     if not os.path.exists(settings["source_path"]):
-        raise TaskProcessingError("The source device (source_path=%s) does not exist" % settings["source_path"])
+        raise TaskSettingsError("The source device (source_path=%s) does not exist" % settings["source_path"])
+    if "lvm" in settings:
+        if not LVM_SIZE_REGEX.match(settings["lvm"]["snapshot_size"]):
+            raise TaskSettingsError("Invalid LVM snapshot size (%s)" % settings["lvm"]["snapshot_size"])
+        vg_name = plumbum.local["lvs"]("--noheadings", "--options", "vg_name", settings["source_path"]).strip()
+        if not vg_name:
+            raise TaskSettingsError("Failed to discover the name of the Volume Group of '{source}' via 'lvs'"
+                                      .format(source=settings["source_path"]))
+        settings["lvm"]["vg_name"] = vg_name
     if not settings["connection_command"]:
         # local transfer
         if not os.path.exists(os.path.dirname(settings["target_path"])):
-            raise TaskProcessingError("The directory of the local target (target_path=%s) does not exist" % \
+            raise TaskSettingsError("The directory of the local target (target_path=%s) does not exist" % \
                     settings["target_path"])
         if not os.path.isdir(settings["target_patch_dir"]):
-            raise TaskProcessingError("The patch directory of the local target (target_patch_dir=%s) does not exist" % \
+            raise TaskSettingsError("The patch directory of the local target (target_patch_dir=%s) does not exist" % \
                     settings["target_patch_dir"])
 
 
 def get_remote_tempfile(connection_command, target, directory):
     cmd_args = shlex.split(connection_command)
     cmd_args.append("mktemp --tmpdir=%s %s-XXXX.bdsync" % (shlex.quote(directory), shlex.quote(os.path.basename(target))))
-    output = plumbum.local[cmd_args[0]](cmd_args[1:])
+    cmd_command = cmd_args.pop(0)
+    output = plumbum.local[cmd_command](cmd_args)
     # remove linebreaks from result
     return output.rstrip("\n\r")
 
@@ -108,25 +117,27 @@ def run_bdsync(source, target, target_patch_dir, connection_command, local_bdsyn
         output_command_args = shlex.split(connection_command)
         output_command_args.append("cat > %s" % shlex.quote(remote_patch_file))
         log.debug("Using remote patch transfer: %s" % str(output_command_args))
-        output_command = plumbum.local[output_command_args[0]][tuple(output_command_args[1:])]
-        patch_size_command = shlex.split(connection_command)
+        output_command_command = output_command_args.pop(0)
+        output_command = plumbum.local[output_command_command][tuple(output_command_args)]
+        patch_size_args = shlex.split(connection_command)
         # "stat --format %s" returns the size of the file in bytes
-        patch_size_command.append("stat --format %%s %s" % shlex.quote(remote_patch_file))
-        patch_size_func = plumbum.local[patch_size_command[0]][tuple(patch_size_command[1:])]
+        patch_size_args.append("stat --format %%s %s" % shlex.quote(remote_patch_file))
+        patch_size_command = patch_size_args.pop(0)
+        patch_size_func = plumbum.local[patch_size_command][tuple(patch_size_args)]
     else:
         remote_command = "%s --server" % shlex.quote(remote_bdsync)
         local_patch_file = tempfile.NamedTemporaryFile(dir=target_patch_dir, delete=False)
         patch_size_func = lambda: os.path.getsize(local_patch_file.name)
         output_command = None
     # run bdsync and handle the resulting states
-    args = []
-    args.append(local_bdsync)
-    if bdsync_args:
-        args.extend(shlex.split(bdsync_args))
-    args.append(remote_command)
-    args.append(source)
-    args.append(target)
-    patch_source = plumbum.local[args[0]][args[1:]]
+    create_patch_args = []
+    create_patch_args.append(local_bdsync)
+    create_patch_args.extend(shlex.split(bdsync_args))
+    create_patch_args.append(remote_command)
+    create_patch_args.append(source)
+    create_patch_args.append(target)
+    create_patch_command = create_patch_args.pop(0)
+    patch_source = plumbum.local[create_patch_command][tuple(create_patch_args)]
     patch_create_start_time = time.time()
     if connection_command:
         chain = patch_source | output_command
@@ -142,13 +153,23 @@ def run_bdsync(source, target, target_patch_dir, connection_command, local_bdsyn
     # everything went fine - now the patch should be applied
     if connection_command:
         patch_source = None
-        bdsync_args = shlex.split(connection_command)
-        bdsync_args.append("%s --patch < %s" % (shlex.quote(remote_bdsync), shlex.quote(remote_patch_file)))
-        apply_patch = plumbum.local[bdsync_args[0]][tuple(bdsync_args[1:])]
+        # remote command: "bdsync [bdsync_args] --patch < PATCH_FILE"
+        remote_command_tokens = []
+        remote_command_tokens.append(remote_bdsync)
+        remote_command_tokens.extend(shlex.split(bdsync_args))
+        remote_command_tokens.append("--patch")
+        remote_command_combined = " ".join([shlex.quote(token) for token in remote_command_tokens])
+        # the input file is added after an unquoted "<"
+        remote_command_combined += " < %s" % shlex.quote(remote_patch_file)
+        # local command: "ssh foo@bar 'REMOTE_COMMAND'"
+        patch_call_args = shlex.split(connection_command)
+        patch_call_args.append(remote_command_combined)
+        patch_call_command = patch_call_args.pop(0)
+        apply_patch = plumbum.local[patch_call_command][tuple(patch_call_args)]
     else:
         local_patch_file.seek(0)
-        patch_source = local_patch_file
-        apply_patch = plumbum.local[local_bdsync]["--patch"]
+        patch_call_args = shlex.split(bdsync_args) + ["--patch"]
+        apply_patch = (plumbum.local[local_bdsync][tuple(patch_call_args)] < local_patch_file)
     log.debug("Applying the patch")
     log.debug("bdsync patching: %s" % str(apply_patch))
     apply_patch()
@@ -159,19 +180,23 @@ def run_bdsync(source, target, target_patch_dir, connection_command, local_bdsyn
         # remove remote patch file
         remove_args = shlex.split(connection_command)
         remove_args.append("rm %s" % shlex.quote(remote_patch_file))
+        remove_command = remove_args.pop(0)
         log.debug("Removing the remote temporary patch file: %s" % str(remove_args))
-        plumbum.local[remove_args[0]](remove_args[1:])
+        plumbum.local[remove_command](remove_args)
     else:
-        os.unlink(bdsync_out.name)
+        os.unlink(local_patch_file.name)
 
 
 def process_task(config):
     settings = load_settings(config)
+    if settings["disabled"]:
+        log.info("Skipping disabled task")
+        return
     validate_settings(settings)
     # everything looks fine - we can start
     if "lvm" in settings:
-        real_source = prepare_lvm_snapshot(settings["source_path"],
-                settings["lvm"]["snapshot_size"], settings["lvm"]["snapshot_name_suffix"])
+        real_source = prepare_lvm_snapshot(settings["source_path"], settings["lvm"]["vg_name"],
+                settings["lvm"]["snapshot_name"], settings["lvm"]["snapshot_size"])
     else:
         real_source = settings["source_path"]
     try:
@@ -179,12 +204,23 @@ def process_task(config):
                 settings["connection_command"], settings["local_bdsync_bin"], settings["remote_bdsync_bin"], settings["bdsync_args"])
     finally:
         if "lvm" in settings:
-            cleanup_lvm_snapshot(real_source)
+            cleanup_lvm_snapshot(settings["lvm"]["vg_name"], settings["lvm"]["snapshot_name"])
+
+
+def prepare_lvm_snapshot(source_path, vg_name, snapshot_name, snapshot_size):
+    log.info("Creating LVM snapshot: {vg_name}/{snapshot_name}".format(vg_name=vg_name, snapshot_name=snapshot_name))
+    plumbum.local["lvcreate"]("--snapshot", "--name", snapshot_name, "--size", snapshot_size, source_path)
+    return "/dev/{vg_name}/{snapshot_name}".format(vg_name=vg_name, snapshot_name=snapshot_name)
+
+
+def cleanup_lvm_snapshot(vg_name, snapshot_name):
+    log.info("Removing LVM snapshot: {vg_name}/{snapshot_name}".format(vg_name=vg_name, snapshot_name=snapshot_name))
+    plumbum.local["lvremove"]("--force", "%s/%s" % (vg_name, snapshot_name))
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Manage one or more bdsync transfers.")
-    parser.add_argument("--log-level", dest="log_level", default="info",
+    parser.add_argument("--log-level", dest="log_level", default="warning",
             choices=("debug", "info", "warning", "error"), help="Output verbosity")
     parser.add_argument("--config", metavar="CONFIG_FILE", dest="config_file",
             default="/etc/bdsync-manager.conf", type=argparse.FileType('r'),
@@ -227,9 +263,12 @@ if __name__ == "__main__":
         tasks = config.sections()
     if not tasks:
         log.warning("There is nothing to be done (no tasks found in config file).")
-    for task in tasks:
-        log_handler.setFormatter(logging.Formatter("[Task '%s'] %%(levelname)s: %%(message)s" % str(task)))
-        try:
-            process_task(config[task])
-        except TaskProcessingError as exc:
-            log.error(str(exc))
+    try:
+        for task in tasks:
+            log_handler.setFormatter(logging.Formatter("[Task '%s'] %%(levelname)s: %%(message)s" % str(task)))
+            try:
+                process_task(config[task])
+            except TaskProcessingError as exc:
+                log.error(str(exc))
+    except KeyboardInterrupt:
+        log.info("Cancelled task")
