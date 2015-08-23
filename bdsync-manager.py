744c13dc231508aa11a3a1269d2ed94a414ee55e
Lars Kruse <devel@sumpfralle.de>
2015-08-23 19:15:19 +0200

    added license details

--- bdsync-manager.py
+++ bdsync-manager.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 """
+    bdsync-manager: maintain synchronization tasks for remotely or locally synchronized blockdevices via bdsync
+
+    Copyright (C) 2015 Lars Kruse <devel@sumpfralle.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 Config file format (example):
 
@@ -18,7 +35,7 @@ Config file format (example):
     lvm_snapshot_size = 5G
     lvm_snapshot_name = bdsync-snapshot
 
-The above values do not represent defaults.
+The above configuration values are merely an example and do not represent bdsync-manager's defaults.
 """
 
 import argparse
