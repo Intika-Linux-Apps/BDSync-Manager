f4d791f2e3f98adb9751ab32d991dc91134bc702
Lars Kruse <devel@sumpfralle.de>
2015-09-13 02:00:15 +0200

    basic pelican website

---
+++ website/pelicanconf.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Lars Kruse'
+SITENAME = u'bdsync-manager'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+# use the filesystem timestamp
+DEFAULT_DATE = "fs"
+
+# Blogroll
+LINKS = (
+        ('bdsync', 'https://github.com/TargetHolding/bdsync/'),
+        ('Python', 'http://python.org/'),
+        ('plumbum', 'http://plumbum.readthedocs.org/'),
+        ('Pelican', 'http://getpelican.com/'),
+)
+
+# Social widget
+SOCIAL = []
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = "notmyidea"
+
+# we turn off some details
+DISPLAY_CATEGORIES_ON_MENU = False
+AUTHOR_SAVE_AS = ""
+YEAR_ARCHIVE_SAVE_AS = ""
+MONTH_ARCHIVE_SAVE_AS = ""
+DAY_ARCHIVE_SAVE_AS = ""
+TAG_SAVE_AS = ""
+CATEGORY_SAVE_AS = ""
+PAGE_LANG_SAVE_AS = ""
